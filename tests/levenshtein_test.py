204b2e7a91b9eb53fb6c74ba2b147d6dbded916a
Daniel Ecer <de-code@users.noreply.github.com>
2019-08-29 12:35:28 +0100

    added example levenshtein distance

---
+++ tests/levenshtein_test.py
@@ -0,0 +1,35 @@
+from sciencebeam_alignment.levenshtein import (
+    get_levenshtein_distance,
+    get_levenshtein_score
+)
+
+
+class TestGetLevenshteinDistance(object):
+    def test_should_return_zero_for_two_equal_sequences(self):
+        assert get_levenshtein_distance('abc', 'abc') == 0
+
+    def test_should_return_one_for_one_substitution(self):
+        assert get_levenshtein_distance('abc', 'axc') == 1
+
+    def test_should_return_one_for_one_deletion(self):
+        assert get_levenshtein_distance('abc', 'ac') == 1
+
+    def test_should_return_one_for_one_insertion(self):
+        assert get_levenshtein_distance('abc', 'abxc') == 1
+
+    def test_should_return_three_for_kitten_vs_sitten(self):
+        assert get_levenshtein_distance('kitten', 'sitting') == 3
+
+
+class TestGetLevenshteinScore(object):
+    def test_should_return_zero_for_two_empty_sequences(self):
+        assert get_levenshtein_score('', '') == 0.0
+
+    def test_should_return_one_for_two_equal_sequences(self):
+        assert get_levenshtein_score('abc', 'abc') == 1.0
+
+    def test_should_return_point_five_for_half_substitution(self):
+        assert get_levenshtein_score('abcd', 'axxd') == 0.5
+
+    def test_should_return_point_five_for_half_deletion(self):
+        assert get_levenshtein_score('abcd', 'ad') == 0.5
