4f3eca81428cc212f95ceb4dd188ea5956678d21
Daniel Ecer <de-code@users.noreply.github.com>
2019-09-03 12:55:37 +0100

    use sys.executable to install numpy (#49)

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import sys
 from setuptools import find_packages, setup, Extension
 
 import sciencebeam_alignment
@@ -16,13 +17,19 @@ with open('README.md', 'r') as f:
 NUMPY_REQUIREMENT = [r.rstrip() for r in REQUIRED_PACKAGES if r.startswith('numpy')][0]
 
 
-def install_numpy_if_not_available():
+def is_numpy_installed():
     try:
         import numpy # noqa pylint: disable=unused-variable
+        return True
     except ImportError:
+        return False
+
+
+def install_numpy_if_not_available():
+    if not is_numpy_installed():
         import subprocess
         print('installing numpy:', NUMPY_REQUIREMENT)
-        subprocess.check_output(['pip', 'install', NUMPY_REQUIREMENT])
+        subprocess.check_output([sys.executable, '-m', 'pip', 'install', NUMPY_REQUIREMENT])
 
 
 def get_numpy_include_dir():
@@ -35,28 +42,39 @@ def get_numpy_include_dir():
 packages = find_packages()
 
 
-setup(
-    name='sciencebeam_alignment',
-    version=sciencebeam_alignment.__version__,
-    install_requires=REQUIRED_PACKAGES,
-    packages=packages,
-    include_package_data=True,
-    description='ScienceBeam Alignment',
-    url='https://github.com/elifesciences/sciencebeam-alignment',
-    license='MIT',
-    keywords="sequence alignment",
-    setup_requires=[
+def setup_package():
+    setup_requires = [
         # Setuptools 18.0 properly handles Cython extensions.
         'setuptools>=18.0',
         'cython'
-    ],
-    ext_modules=[
-        Extension(
-            'sciencebeam_alignment.align_fast_utils',
-            sources=['sciencebeam_alignment/align_fast_utils.pyx']
-        ),
-    ],
-    include_dirs=[get_numpy_include_dir()],
-    long_description=long_description,
-    long_description_content_type='text/markdown'
-)
+    ]
+
+    if not setup_requires:
+        # only add numpy if not already installed (see scipy setup.py)
+        setup_requires = setup_requires + [NUMPY_REQUIREMENT]
+
+    setup(
+        name='sciencebeam_alignment',
+        version=sciencebeam_alignment.__version__,
+        install_requires=REQUIRED_PACKAGES,
+        packages=packages,
+        include_package_data=True,
+        description='ScienceBeam Alignment',
+        url='https://github.com/elifesciences/sciencebeam-alignment',
+        license='MIT',
+        keywords="sequence alignment",
+        setup_requires=setup_requires,
+        ext_modules=[
+            Extension(
+                'sciencebeam_alignment.align_fast_utils',
+                sources=['sciencebeam_alignment/align_fast_utils.pyx']
+            ),
+        ],
+        include_dirs=[get_numpy_include_dir()],
+        long_description=long_description,
+        long_description_content_type='text/markdown'
+    )
+
+
+if __name__ == '__main__':
+    setup_package()
